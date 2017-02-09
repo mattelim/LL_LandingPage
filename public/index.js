57c32f9d61fa89e986e741b9719b996a3c44f7d4
Matthew Lim <ke_wei_matthew_lim@brown.edu>
2017-02-09 23:40:02 +0800

    added EZ-Link, mobile doesn't move

--- public/index.js
+++ public/index.js
@@ -1,12 +1,17 @@
+mql = window.matchMedia("only screen and (min-device-width: 320px) and (max-device-width: 568px)");
 
 $("body").mousemove(function(e) {
+  if (!mql.matches) {
     var x = -(e.pageX + this.offsetLeft) / 45 + 60;
     var y = -(e.pageY + this.offsetTop) / 45 + 60;
     $(this).css('background-position', x + '% ' + y + '%');
+  }
 });
 
 $("#mainContainer").mousemove(function(e) {
+  if (!mql.matches) {
     var x = -(e.pageX + this.offsetLeft) / 15 + 80;
     var y = -(e.pageY + this.offsetTop) / 15 + 80;
     $("#mainContainer").css('background-position', x + '% ' + y + '%');
+  }
 });
